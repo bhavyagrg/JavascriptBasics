efa3a608c599cb859dfe5b17a90417ac62445e90
Bhavya Garg <bebhavyagarg@gmail.com>
2023-09-03 12:22:17 +0530

    first commit

---
+++ Operators.js
@@ -0,0 +1,12 @@
+a = 2;
+b = 3;
+
+console.log("a + b = ",a + b);
+console.log("a - b = ",a - b);
+console.log("a * b = ",a * b);
+console.log("a / b = ",a / b);
+console.log("a ** b = ",a ** b);
+console.log("a % b = ",a % b);
+console.log("a++ : ",a++);
+console.log("a: ",a);
+console.log("++a: ",++a);
---
+++ PrimitivesObjects.js
@@ -0,0 +1,20 @@
+// Primitives in js
+let a = null;
+let b = 24;
+let c = true;
+let d = "Bhavya Garg"
+let e = BigInt("83253575751") + BigInt("6");
+let f = Symbol("I am a nice symbol");
+let g = undefined;
+
+console.log(a,b,c,d,e,f,g)
+console.log(typeof(d))
+
+// objects or non primitive data types in js
+const items = {
+    "name": "Bhavya Garg",
+    age:19,
+    "dob":"010104"
+}
+
+console.log(items["name"])
