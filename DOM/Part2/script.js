086d0dfcfa59076ccb7a468e43f4acd450aa1237
Bhavya Garg <bebhavyagarg@gmail.com>
2023-09-04 14:29:11 +0530

    Added part 2 to DOM

---
+++ DOM/Part2/script.js
@@ -0,0 +1,8 @@
+document.getElementsByTagName("li")[2].style.color = "blue";
+document.getElementsByClassName("list")[0].style.color = "orange";
+document.getElementById("l").style.color = "green";
+document.querySelector("a").style.color = "black";
+document.querySelectorAll("a")[0].style.fontSize = "50px";
+
+document.getElementsByTagName("button")[0].style.backgroundColor = "pink";
+
